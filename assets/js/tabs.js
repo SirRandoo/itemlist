4591de50ec36a2619587dfd793423c78683974d1
NekoRivera <85807519+NekoRivera@users.noreply.github.com>
2021-06-23 21:39:34 -0400

    Delete tabs.js

--- assets/js/tabs.js
+++
@@ -1,9 +0,0 @@
-function changeTab(evt, category) {
-    var tabContent = document.getElementsByClassName("tabContent");
-    var tabLinks = document.getElementsByClassName("tabLinks");
-
-    [...tabContent].forEach(element => { element.style.display = "none"; });
-    [...tabLinks].forEach(element => { element.className = element.className.replace(" active", ""); })
-    document.getElementById(category).style.display = "block";
-    evt.currentTarget.className += " active";
-}
